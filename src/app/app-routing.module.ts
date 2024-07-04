65dc1727cd4baef35bcc08945dc30f05279ea2b1
DanielOcloo <83448178+DanielOcloo@users.noreply.github.com>
2024-07-04 14:19:56 +0000

    Updated Sign up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginPageComponent } from './login-page/login-page.component';
+import { LoginPageComponent } from './main-pages/login-page/login-page.component';
+import { SignupPageComponent } from './main-pages/signup-page/signup-page.component';
+import { ForgotPasswordComponent } from './main-pages/forgot-password/forgot-password.component';
+import { DashboardComponent } from './main-pages/dashboard/dashboard.component';
 
 const routes: Routes = [
   {path:"", component:LoginPageComponent},
+  {path:"signup", component:SignupPageComponent},
+  {path:"login/forgotpassword", component:ForgotPasswordComponent},
+  {path:"login/dashboard", component:DashboardComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { LoginPageComponent } from './login-page/login-page.component';
+import { LoginPageComponent } from './main-pages/login-page/login-page.component';
 import { TextFieldComponent } from './reusable-components/text-field/text-field.component';
 import { AuthCarouselComponent } from './reusable-components/auth-carousel/auth-carousel.component';
 import { PasswordFieldComponent } from './reusable-components/password-field/password-field.component';
 import { EmailTextFieldComponent } from './reusable-components/email-text-field/email-text-field.component';
+import { SignupPageComponent } from './main-pages/signup-page/signup-page.component';
+import { ForgotPasswordComponent } from './main-pages/forgot-password/forgot-password.component';
+import { DashboardComponent } from './main-pages/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
@@ -18,6 +21,9 @@ import { EmailTextFieldComponent } from './reusable-components/email-text-field/
     AuthCarouselComponent,
     PasswordFieldComponent,
     EmailTextFieldComponent,
+    SignupPageComponent,
+    ForgotPasswordComponent,
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
